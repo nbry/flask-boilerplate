236c62d9405434ddb573f0b6d0bd9a7822e8e212
nbry <nwbryant@umich.edu>
2021-05-09 21:36:44 -0700

    Add general helper to a route

--- project/routes/general_routes.py
+++ project/routes/general_routes.py
@@ -5,8 +5,11 @@ Code for routes should be expressive.
 Functions, logic, algorithms etc. should be in a separate file.
 """
 
-from flask import jsonify
-from . import general_api_blueprint
+from flask import Blueprint, jsonify
+from project.helpers import General
+
+# Create a blueprint. Import this into project.routes.__init__.py
+general_api_blueprint = Blueprint('general_api', __name__)
 
 
 @general_api_blueprint.route('/ping')
@@ -18,3 +21,13 @@ def check_api_status():
     """
 
     return jsonify({"success": True})
+
+
+@general_api_blueprint.route('/helper')
+def check_helper_import():
+    """
+    Default HELPER route that imports the default general
+    helper class, and returns a message.
+    """
+    message = General.sample_helper()
+    return jsonify({"message": message})
