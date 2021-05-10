f8a251e696b3520ebf84513371134cd01f9233fe
nbry <nwbryant@umich.edu>
2021-05-09 21:35:12 -0700

    Create basic seed database file

---
+++ seed.py
@@ -0,0 +1,14 @@
+""" Seed file to make sample data for db. """
+
+from project.all_extensions import db
+
+# MUST import app for seeding to work
+# noinspection PyUnresolvedReferences
+from app import app
+
+# Create all tables
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+print("Tables Created!")
