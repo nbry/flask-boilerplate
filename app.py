b3223069ad5050ace4582032f0a313d4f2dca241
nbry <nwbryant@umich.edu>
2021-05-09 22:16:34 -0700

    Write better clarification in app.py

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from project import create_app
 
-# Separate config settings for different instances of a flask app is
-# a great way to aid in setting up testing fixtures in Pytest.
+# This project is designed to use a directory named /instance for separate
+# configuration settings. You can make configurations for applications in
+# different environments.
 
-# By default.  the configuration files will barely differ.
+# By using the create_app method, we are creating a Flask instance and feeding
+# a config (.cfg) file into it. By default, this Flask app will run on the
+# development configuration settings. The create_app factory method can be
+# found in /project/__init__.py.
+
+# By default the configuration files will barely differ.
 # When deploying this project, you may want to set up a separate production
 # configuration file that utilizes environment variables.
+
 config_settings = "development.cfg"
 
 # Instantiate the app. This particular line will run when using the
